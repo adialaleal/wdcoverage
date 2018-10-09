e806e761af1d7a36c7d38fbb2c31afd9e32adece
Lucas Leal <lealadiala@gmail.com>
2018-10-09 17:51:51 -0300

    Change of default route - try #02.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :toners
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/", to: 'toners#index'
+  get "/", to: 'toner#index'
 end
